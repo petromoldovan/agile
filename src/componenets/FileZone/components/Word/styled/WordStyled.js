89fbbe3cf1532ac15a957cacec86194fe4c31a02
petromoldovan <petro.moldovan@dcso.de>
2019-10-19 17:26:17 +0200

    basic styling

--- src/componenets/FileZone/components/Word/styled/WordStyled.js
+++ src/componenets/FileZone/components/Word/styled/WordStyled.js
@@ -3,7 +3,9 @@ import React from "react"
 
 const WordStyled = styled.div`
   margin-right: 5px;
+  margin-bottom: 5px;
   cursor: pointer;
+  max-height: 25px;
   ${({isSelected}) => isSelected && 'color: red;'}
   ${({ isBold }) => isBold && 'font-weight: 900;'}
   ${({ isItalic }) => isItalic && 'font-style: italic;'}
